d40e417a19ab5902898c53f71e95b0d2e10dbf1c
van0509 <free001@vip.qq.com>
2018-09-06 10:12:50 +0800

    修正验证码显示

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,42 +4,48 @@ from django.contrib.auth.models import AbstractUser
 
 from datetime import datetime
 
+
 # Create your models here.
 
 
 class UserProfile(AbstractUser):
-    nick_name=models.CharField(max_length=50,verbose_name=u'昵称',default=u'')
-    birthday=models.DateField(verbose_name=u'生日',null=True,blank=True)
-    gender=models.CharField(choices=(('male',u'男'),('demale',u'女')),default=u'female',max_length=7)
-    address=models.CharField(max_length=100,default=u'')
-    mobile=models.CharField(max_length=11,null=True,blank=True)
-    image=models.ImageField(upload_to='image/%Y/%m',default=u'image/default.png',max_length=100)
+    nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default=u'')
+    birthday = models.DateField(verbose_name=u'生日', null=True, blank=True)
+    gender = models.CharField(choices=(('male', u'男'), ('demale', u'女')), default=u'female', max_length=7)
+    address = models.CharField(max_length=100, default=u'')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to='image/%Y/%m', default=u'image/default.png', max_length=100)
 
     class Meta:
-        verbose_name=u'用户信息'
-        verbose_name_plural=verbose_name
+        verbose_name = u'用户信息'
+        verbose_name_plural = verbose_name
+
     def __unicode__(self):
         return self.username
 
+
 class EmailVerifyRecord(models.Model):
-    code=models.CharField(max_length=20,verbose_name=u'验证码')
-    email=models.EmailField(max_length=50,verbose_name=u'邮箱')
-    send_type=models.CharField(choices=(('register',u'注册'),('forget',u'找回密码')),max_length=110,verbose_name=u'验证码类型')
-    send_time=models.DateTimeField(default=datetime.now,verbose_name=u'发送时间')
+    code = models.CharField(max_length=20, verbose_name=u'验证码')
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱')
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=110,
+                                 verbose_name=u'验证码类型')
+    send_time = models.DateTimeField(default=datetime.now, verbose_name=u'发送时间')
 
     class Meta:
-        verbose_name=u'邮箱验证码'
-        verbose_name_plural=verbose_name
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
 
-    def __unicode__(self):
-        return u'{0}({1})'.format(self.code,self.email)
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
 class Banner(models.Model):
-    title=models.CharField(max_length=100,verbose_name=u'标题')
-    image=models.ImageField(upload_to='banner/%Y/%m',verbose_name=u'轮播图',max_length=100)
-    url=models.CharField(max_length=200,verbose_name=u'访问地址')
-    index=models.IntegerField(default=100,verbose_name=u'顺序')
-    add_time=models.DateTimeField(default=datetime.now,verbose_name=u'添加时间')
+    title = models.CharField(max_length=100, verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=u'轮播图', max_length=100)
+    url = models.CharField(max_length=200, verbose_name=u'访问地址')
+    index = models.IntegerField(default=100, verbose_name=u'顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
-        verbose_name=u'轮播图'
-        verbose_name_plural=verbose_name
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
